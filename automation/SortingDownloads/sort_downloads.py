830537b2904b94338e21a961126e06ccdf04d868
snakenotgreen <endercraft126@gmail.com>
2025-10-16 22:43:48 +0300

    added context menu to choose folder

--- automation/SortingDownloads/sort_downloads.py
+++ automation/SortingDownloads/sort_downloads.py
@@ -1,6 +1,8 @@
 import hashlib
 import os
 import sys
+from PyQt6.QtWidgets import QApplication, QWidget, QFileDialog
+
 #class filemanager
 file_type_categories = {
     'jpeg': 'Зображення', 'jpg': 'Зображення', 'png': 'Зображення',
@@ -32,8 +34,14 @@ file_type_categories = {
     'ttf': 'Шрифти', 'otf': 'Шрифти', 'woff': 'Шрифти', 'woff2': 'Шрифти',
     'torrent': 'Торренти', 'log': 'Лог-файли', 'bak': 'Резервні копії'
 }
-# a = input("Введіть шлях до вашої теки")
-path = "D:\Загрузки".replace("\\", '/')
+
+app = QApplication([])
+window = QWidget()
+window.setWindowTitle('FirstApp')
+file = QFileDialog()
+fileName, _ = QFileDialog.getOpenFileName(parent=window)
+if fileName:
+    path = os.path.dirname(fileName)
 
 data = os.listdir(path) #всі файли теки
 data_and_ext = {} #назви файлів і розширення
@@ -43,50 +51,64 @@ items_to_transfer = []
 folders_for_transfer = []
 
 #отримуємо список всіх потрібних розширень для створення
+def get_ext():
+    global data_and_ext
+    for x in data:
+        if '.' not in x:
+            continue
+        extension = x.split('.')[-1] #extension
+        items_to_transfer.append(x)
+        if extension in data_and_ext:
+            data_and_ext[extension].append(x)
+        else:
+            data_and_ext[extension] = [x]
 
-for x in data:
-    if '.' not in x:
-        continue
-    extension = x.split('.')[-1] #extension
-    items_to_transfer.append(x)
-    if extension in data_and_ext:
-        data_and_ext[extension].append(x)
-    else:
-        data_and_ext[extension] = [x]
+def get_folders_to_create_and_transfer():
+    global fold_to_create, folders_for_transfer
+    for key, value in data_and_ext.items():
+        fold_to_create.update({file_type_categories.get(key)})
 
-for key, value in data_and_ext.items():
-    fold_to_create.update({file_type_categories.get(key)})
-
-for folder in fold_to_create:
-    folders_for_transfer.append(folder)
+    for folder in fold_to_create:
+        folders_for_transfer.append(folder)
 
 
-#видаляємо папки для створення якщо вони вже є в завантаженнях
-try:
-    with os.scandir(path) as entries:
-        for entry in entries:
-            if entry.is_dir() and entry.name in fold_to_create:
-                created_dirs.append(entry.name)
-                fold_to_create.remove(entry.name)
-except KeyError:
-    pass
-except FileNotFoundError:
-    print(f"ПОМИЛКА: Папку '{path}' не знайдено!")
-except Exception as e:
-    print(f"Сталася невідома помилка: {e}")
+    #видаляємо папки для створення якщо вони вже є в завантаженнях
+    try:
+        with os.scandir(path) as entries:
+            for entry in entries:
+                if entry.is_dir() and entry.name in fold_to_create:
+                    created_dirs.append(entry.name)
+                    fold_to_create.remove(entry.name)
+    except KeyError:
+        pass
+    except FileNotFoundError:
+        print(f"ПОМИЛКА: Папку '{path}' не знайдено!")
+    except Exception as e:
+        print(f"Сталася невідома помилка: {e}")
+
 
 #створюємо папки, якщо їх немає
-try:
-    if fold_to_create:
-        for folder in fold_to_create:
-            os.mkdir(f'{path}/{folder}')
-except FileExistsError:
-    pass
+def creating_folders(path1):
+    try:
+        if fold_to_create:
+            for folder in fold_to_create:
+                os.mkdir(f'{path1}/{folder}')
+    except FileExistsError:
+        pass
 
-try:
-    for extens, files in data_and_ext.items():
-        fold = file_type_categories.get(extens)
-        for file in files:
-            os.rename(f'{path}/{file}',f'{path}/{fold}/{file}')
-except FileNotFoundError:
-    pass
+def transfer_files(path1):
+    try:
+        for extens, files in data_and_ext.items():
+            fold = file_type_categories.get(extens)
+            for file in files:
+                os.rename(f'{path1}/{file}',f'{path1}/{fold}/{file}')
+    except FileNotFoundError:
+        pass
+
+
+get_ext()
+get_folders_to_create_and_transfer()
+creating_folders(path)
+transfer_files(path)
+
+
